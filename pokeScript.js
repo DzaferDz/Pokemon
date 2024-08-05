d6fe1e43e2a1aa8f7eeb15487788aade6b424e24
Dzafer <dzaferdzaferovicwd@gmail.com>
2024-08-05 10:50:31 +0200

    Check for mistake in data fetch

--- pokeScript.js
+++ pokeScript.js
@@ -10,5 +10,54 @@ const defense = document.getElementById('defense');
 const specialAttack = document.getElementById('special-attack');
 const specialDefense = document.getElementById('special-defense');
 const speed = document.getElementById('speed');
-const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+
+const getPokemon = async () => {
+try  {
+  const nameOrId = searchInput.value().toLowerCase();
+    const res = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${nameOrId}`);
+    const data = await res.json()
+    pokemonName.textContent = `${data.name.toUpperCase()}`;
+    pokemonID.textContent = `#${data.id}`;
+    weight.textContent = `${data.weight}`;
+    height.textContent = `${data.height}`;
+    spriteContainer.innerHTML = `
+      <img id="sprite" src="${data.sprites.front_default}" alt="${data.name} front default sprite">
+    `;
+
+    // Set stats
+    hp.textContent = data.stats[0].base_stat;
+    attack.textContent = data.stats[1].base_stat;
+    defense.textContent = data.stats[2].base_stat;
+    specialAttack.textContent = data.stats[3].base_stat;
+    specialDefense.textContent = data.stats[4].base_stat;
+    speed.textContent = data.stats[5].base_stat;
+
+    // Set types
+    types.innerHTML = data.types.map(obj => `<span class="type ${obj.type.name}">${obj.type.name}</span>`)
+      .join('');
+  } catch (err) {
+    resetDisplay();
+    alert('Pokémon not found');
+    console.log(`Pokémon not found: ${err}`);
+  }
+}
+const resetDisplay = () => {
+    const sprite = document.getElementById('sprite');
+    if (sprite) sprite.remove();
+  
+    // reset stats
+    pokemonName.textContent = '';
+    pokemonID.textContent = '';
+    types.innerHTML = '';
+    height.textContent = '';
+    weight.textContent = '';
+    hp.textContent = '';
+    attack.textContent = '';
+    defense.textContent = '';
+    specialAttack.textContent = '';
+    specialDefense.textContent = '';
+    speed.textContent = '';
+  };
+  searchButton.addEventListener("click",getPokemon);
